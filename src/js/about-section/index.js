8165453a19ce181ebff26ae23fd11e959c7ffa91
Joshua Fielding <Joshua.Fielding@fairfaxmedia.com.au>
2020-05-14 15:39:32 +1000

    neatening

--- src/js/about-section/index.js
+++ src/js/about-section/index.js
@@ -1,63 +1,56 @@
-$(".italic-two").on("mouseover", function() {
+$(".italic-two").on("mouseover", function () {
   $(".techno").css("opacity", "1");
   $(".standard").css("opacity", "0");
   $("a").css("opacity", "0");
 });
 
-$(".italic-two").on("mouseout", function() {
+$(".italic-two").on("mouseout", function () {
   $(".techno").css("opacity", "0");
   $(".standard").css("opacity", "1");
   $("a").css("opacity", "1");
 });
 
-$(".italic-three").on("mouseover", function() {
+$(".italic-three").on("mouseover", function () {
   $(".raver").css("opacity", "1");
   $(".standard").css("opacity", "0");
   $("a").css("opacity", "0");
 });
 
-$(".italic-three").on("mouseout", function() {
+$(".italic-three").on("mouseout", function () {
   $(".raver").css("opacity", "0");
   $(".standard").css("opacity", "1");
   $("a").css("opacity", "1");
 });
 
-$(".italic-four").on("mouseover", function() {
+$(".italic-four").on("mouseover", function () {
   $(".gabber").css("opacity", "1");
   $(".standard").css("opacity", "0");
   $("a").css("opacity", "0");
 });
 
-$(".italic-four").on("mouseout", function() {
+$(".italic-four").on("mouseout", function () {
   $(".gabber").css("opacity", "0");
   $(".standard").css("opacity", "1");
   $("a").css("opacity", "1");
 });
 
-$(".italic-five").on("mouseover", function() {
+$(".italic-five").on("mouseover", function () {
   $(".acid").css("opacity", "1");
   $(".standard").css("opacity", "0");
   $("a").css("opacity", "0");
 });
 
-$(".italic-five").on("mouseout", function() {
+$(".italic-five").on("mouseout", function () {
   $(".acid").css("opacity", "0");
   $(".standard").css("opacity", "1");
   $("a").css("opacity", "1");
 });
 
-// colour wheel
-
-// // 1. on hover, create a new div with a class of pink-dots
-$(".colour-wheel").hover(() => {
-  $("").toggleClass("on");
+const colourWheel = document.querySelector(".colour-wheel");
+const circle = document.querySelector(".circle");
+colourWheel.addEventListener("mouseover", () => {
+  circle.classList.add("circle-active", "fade");
 });
-
-// GRAPHIC DESIGNER...
-// To fade out text link
-// 1. toggle a class of "faded"
-// 2. in css, add styles to .faded
-$(".colour-wheel").hover(() => {
-  $(".fade").toggleClass("faded");
-  $(".circle").toggleClass("loading");
+colourWheel.addEventListener("mouseout", () => {
+  circle.classList.remove("circle-active");
 });
